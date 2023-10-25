e9cc7d4ed6e661556bac10874d66392638327c99
sallyjellyy <min27604@gmail.com>
2023-10-26 02:33:32 +0900

    Add mongodb reactive dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 //  implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.0.4")
 
   implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 }
 
 tasks.withType<KotlinCompile> {
